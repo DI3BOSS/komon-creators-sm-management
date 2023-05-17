bea42dc45c1a7d14e656db94a78e9a4f2af6c2e9
DI3BOSS <diegofbermudez@gmail.com>
2023-05-18 00:55:45 +0200

    Add an instagram mocked user accountdata

---
+++ src/data/instagramAccount.ts
@@ -0,0 +1,11 @@
+import { SocialMediaStructure } from "./types";
+
+const instagramAccount: SocialMediaStructure = {
+  networkIcon: "media/icons/instagram.svg",
+  networkTitle: "Instagram",
+  username: "dbeet",
+  avatar:
+    "https://scontent-bcn1-1.cdninstagram.com/v/t51.2885-19/328455781_651748266617500_8647717706643645460_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-bcn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=f1eIaJWer4wAX_TZ3CC&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfBFQXdmzFhfwFPjjecKqA__d5UG9NHG1A8K_MYAG37qnw&oe=646AEBB4&_nc_sid=a1ad6c",
+};
+
+export default instagramAccount;
