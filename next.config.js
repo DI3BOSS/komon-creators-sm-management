7ba539e24ad4833ca73c8bc2543fa21fc9a551d0
DI3BOSS <diegofbermudez@gmail.com>
2023-05-18 00:26:15 +0200

    Edit next.config in order to allow remotePatterns from instagram origin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent-bcn1-1.cdninstagram.com",
+        port: "",
+        pathname: "/v/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
