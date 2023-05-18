4348b3f1eb01135549fd756163ec64a647e7be6d
DI3BOSS <diegofbermudez@gmail.com>
2023-05-18 15:02:15 +0200

    Implement a mock list of instagram posts for rendering purposes

--- src/data/instagramAccount.ts
+++ src/data/instagramAccount.ts
@@ -6,6 +6,24 @@ const instagramAccount: SocialMediaStructure = {
   username: "dbeet",
   avatar:
     "https://scontent-bcn1-1.cdninstagram.com/v/t51.2885-19/328455781_651748266617500_8647717706643645460_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-bcn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=f1eIaJWer4wAX_TZ3CC&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfBFQXdmzFhfwFPjjecKqA__d5UG9NHG1A8K_MYAG37qnw&oe=646AEBB4&_nc_sid=a1ad6c",
+  postList: [
+    {
+      image:
+        "https://scontent-bcn1-1.cdninstagram.com/v/t51.2885-15/273136395_621788122229795_4930049556129670927_n.webp?stp=c149.0.781.781a_dst-jpg_e35_s240x240&_nc_ht=scontent-bcn1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=cIvTwC2o-0wAX_Y4WP0&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAiHiZpVreroqITcSBQiygnQRZXLC9BaEd9Edmlci_jDA&oe=646AF836&_nc_sid=f70a57",
+      description:
+        "dbeet Like my favourite song going round and round in my head.",
+    },
+    {
+      image:
+        "https://scontent-bcn1-1.cdninstagram.com/v/t51.2885-15/275909608_367885481670676_2509090435307893157_n.webp?stp=c0.135.1080.1080a_dst-jpg_e35_s240x240&_nc_ht=scontent-bcn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=VIQg96Qd9vIAX_EIm6Z&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfCqtxUkpN66YpvJReP1UHKLN65LQXcNozjW9isIRanJFQ&oe=6469550A&_nc_sid=f70a57",
+      description: "dbeet A modern Wanderer above the Sea of Fog.",
+    },
+    {
+      image:
+        "https://scontent-bcn1-1.cdninstagram.com/v/t51.2885-19/328455781_651748266617500_8647717706643645460_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-bcn1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=f1eIaJWer4wAX_TZ3CC&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfBFQXdmzFhfwFPjjecKqA__d5UG9NHG1A8K_MYAG37qnw&oe=646AEBB4&_nc_sid=a1ad6c",
+      description: "dbeet Summertime!🌡️☀️🌊",
+    },
+  ],
 };
 
 export default instagramAccount;
--- src/data/types.ts
+++ src/data/types.ts
@@ -1,14 +1,13 @@
-export interface SocialMediaStructure {
-  networkIcon: string;
-  networkTitle: string;
-  username: string;
-  avatar: string;
-  postList?: React.JSX.Element;
-}
-
 export interface PostStructure {
   image: string;
   description: string;
 }
 
 export type PostListStructure = PostStructure[];
+export interface SocialMediaStructure {
+  networkIcon: string;
+  networkTitle: string;
+  username: string;
+  avatar: string;
+  postList?: PostListStructure;
+}
